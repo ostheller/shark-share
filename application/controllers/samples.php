7e158bf0fb2b0b2ebd6494d413b799888c4c0327
Molly Ostheller <molly.ostheller@gmail.com>
2016-03-18 11:06:49 -0700

    Adding methods to controllers
    Starting to outline the functions that we’re going to need to write

--- application/controllers/samples.php
+++ application/controllers/samples.php
@@ -2,9 +2,53 @@
 
 class Samples extends CI_Controller {
 
-	// 
-	public function index()
+// user enters a search
+	public function search()
 	{
-		$this->load->view('user_dashboard');
+		$post = $this->input->post();
+		$data = $this->sample->search($post);
+		$this->load->view('search', array($data));
 	}
+
+// user clicks browse, pulls the data and loads the search results page
+	public function browse()
+	{
+		$this->load->view('search');
+	}
+
+// user wants to see a collection, pulls the data and loads the collection page. 
+// could be theirs or anothers' collection, depends on what we pass it
+	public function view_collection()
+	{
+		$this->load->view('user_collection');
+	}
+
+// user wants to see a sample's profile page
+	public function view_sample()
+	{
+		$this->load->view('sample_profile');
+	}
+
+// user requests a sample, adding it to the 'checkout cart' that we have running
+	public function request()
+	{
+	}
+
+// this function will be running to query the 'checkout cart' to see what the user has in their basket
+	public function check_cart()
+	{
+
+	}
+
+// uploading samples to a user's collection, if successful redirect to upload_success, otherwise no
+	public function upload()
+	{
+		
+	}
+
+// uploading samples to a user's collection, if successful redirect to 
+	public function upload_success()
+	{
+		$this->load->view('user_collection');
+	}	
 }
--- application/controllers/users.php
+++ application/controllers/users.php
@@ -2,45 +2,52 @@
 
 class Users extends CI_Controller {
 
-	// loads www.example.com/ on intial load
+// loads www.example.com/ on intial load
 	public function index()
 	{
 		$this->load->view('landing_page');
 	}
 
-	// user logs in
+// user logs in
 	public function login()
 	{
 		$this->load->view('user_dashboard');
 	}
 
-	// user signs up, returns to landing page
+// user signs up, returns to landing page
 	public function sign_up()
 	{
 		$this->load->view('landing_page');
 	}
 
-	// login successful, loads the user dashboard page
+// login successful, loads the user dashboard page
 	public function login_success()
 	{
 		$this->load->view('user_dashboard');
 	}
 
-	// login successful, user is an admin, loads the admin dashboard page
+// login successful, user is an admin, loads the admin dashboard page
 	public function admin_login_success()
 	{
 		$this->load->view('admin_dashboard');
 	}
 
-	// load the user dashboard page when user is logged in
+// load the user dashboard page when user is logged in
 	public function view_user_dashboard()
 	{
 		$this->load->view('user_dashboard');
 	}
 
-	// load the admin dashboard page when an admin user is logged in
+// load the admin dashboard page when an admin user is logged in
 	public function view_admin_dashboard()
 	{
 		$this->load->view('user_dashboard');
+	}
+
+// user wants to see a profile, pulls the data and loads the profile page. 
+// could be theirs or anothers' profile, depends on what we pass it
+	public function view_user()
+	{
+		$this->load->view('user_profile');
 	}
 }
