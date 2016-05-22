b295f56626d5e537ecb64b42437e4c08e6a1381e
Molly Ostheller <molly.ostheller@gmail.com>
2016-05-21 18:01:26 -0700

    Updated the css to make the image smaller in the footer

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,6 +1,6 @@
 		<footer class="container-fluid text-center">
 		<a target="_blank" href="http://www.saveourseas.com/"> 
-          <img id = "sos-image" src="/assets/images/soslogo.png" alt= "soslogo">
+          <img id="sos-image" src="/assets/images/soslogo.png" alt= "soslogo">
         </a>
 		  <p>Copyright 2016 Shark Share Global</p>
 		</footer>
--- application/views/setup_profile.php
+++ application/views/setup_profile.php
@@ -1,19 +1,18 @@
 <div class="container-fluid text-center">    
-  <div class="row content">
-    <!--<div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-    </div>-->
-    <div class="col-sm-8 text-left">
+  <div class="row content" id="forms">
+    <div class="col-sm-12 text-left">
     <h1>Hello and Welcome, <?=$user['first_name']?> <?=$user['last_name']?>!</h1>
+    <hr>
+    <div id="setup_profile_block" class="info_block">
     <h3>Set Up Profile:</h3>
-      <form id="setup" method="post" action="setup_user/submit">
+      <form id="setup" method="post" action="setup/submit">
         <h5>Choose a username</h5>
+        <input type="hidden" value="<?=$user['id']?>" name="id">
         <input type="text" name="username" value="<?php echo set_value('Username'); ?>" size="50" />
         <?php echo form_error('username'); ?>
         <h5>Password</h5>
-        <input type="password" name="password" value="" size="50" />
+        <input type="password" name="password" value="" id="password" size="50" />
+        <span id="result"></span>
         <?php echo form_error('password'); ?>
         <h5>Password Confirm</h5>
         <input type="password" name="passconf" value="" size="50" />
@@ -21,10 +20,13 @@
         <h5>Introduce Yourself</h5>
         <textarea name="about" value="<?php echo set_value('About'); ?>" cols="50" rows="4"></textarea>
         <?php echo form_error('about'); ?>
-        <div><input type="submit" value="Submit" /></div>
+        <div><button id ="setup_pasword" class="btn btn-primary submit">Submit</button></div>
+        <div><span id="password_result"></span></div>
       </form>
+    </div>
+    <div id="verify_info_block" class="info_block">
       <h3>Verify Your Information:</h3>
-        <form id= "verify" method="post" action='user/'.<?=$user['id']?>.'/update')>
+        <form id= "verify" method="post" action='user/update')>
           <h5>First Name</h5>
           <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>" size="50" />
           <?php echo form_error('first_name'); ?>
@@ -53,13 +55,17 @@
           <h5>Country</h5>
           <select name="country">
             <?php foreach ($countries as $country) { ?>
-              <option value="<?= $country['id'] ?>" <?php if ($country['id'] == $user['country_id']) {echo 'selected';}?>><?= $country['code'] ?> (<?= $country['name'] ?>)</option>
+              <option value="<?= $country['id'] ?>" <?php if ($country['id'] == intval($user['country_id'])) {echo 'selected';}?>><?= $country['code'] ?> (<?= $country['name'] ?>)</option>
             <?php } ?>
           </select>
-          <div><input type="submit" value="Submit Changes" /></div>
+          <div><button class="btn btn-primary submit disabled" id="verify_changes">Verify</button></div>
+            <div><span id="verify_result"></span></div>
           </form>
+        </div>
+      <div class="info_block">
       <h3>Site Preferences:</h3>
-      <form id="tags" method="post" action="/setup_user/tags">
+      <form id="tags" method="post" action="/setup/tags">
+        <input type="hidden" value="<?=$user['id']?>" name="id">
         <h5>Choose a preferred genus</h5>
           <input type="text" name="genus" id="genus" class="fitfont">
         <h5>Choose a preferred species</h5>
@@ -71,17 +77,13 @@
               <option value="<?= $sample_type['id'] ?>"><?= $sample_type['type'] ?></option>
             <?php } ?>
           </select>
-        <input type="submit" value="Submit"></input>
+          <div><button class="btn btn-primary submit disabled" id="submit_tags">Submit</button></div>
+          <div><span id="tags_result"></span></div>
       </form>
+      </div>
+      <hr>
+      <button class="disabled btn-lg btn-success btn" id="success_button">Enter Site</button>
     </div>
-    <!--<div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>Content</p>
-      </div>
-      <div class="well">
-        <p>Content</p>
-      </div>
-    </div>-->
     </div>
 </div>
 <script src="<?= base_url();?>/assets/js/custom/setup_profile.js"></script>
